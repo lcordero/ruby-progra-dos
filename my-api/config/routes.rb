738a71c830dc9a8aeacbd82f0ab248d3540037ce
sebas-v <jsvale.guerrero@gmail.com>
2019-06-20 18:47:53 +0000

    endpoint

--- my-api/config/routes.rb
+++ my-api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :facturas do
+    		resources :productos
+	end
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos do
     resources :items
   end

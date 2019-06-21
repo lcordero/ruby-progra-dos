bbd0f520d565abb1d2327f22d17df92aef28c08e
sebas-v <jsvale.guerrero@gmail.com>
2019-06-21 01:16:19 +0000

    clases

--- my-api/config/routes.rb
+++ my-api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	resources :facturas do
-    		resources :productos
+ 		resources :productos
 	end
 	# For details on the DSL available within this file, see 	http://guides.rubyonrails.org/routing.html
   	resources :todos do
--- my-api/db/migrate/20190620183319_create_productos.rb
+++ my-api/db/migrate/20190620183319_create_productos.rb
@@ -1,12 +1,12 @@
 class CreateProductos < ActiveRecord::Migration[5.2]
-	def change
+	def changes
     		create_table :productos do |t|
-		t.references :factura, foreign_key: true
-     		t.string :nombre
-     		t.integer :cantidad
-     		t.integer :precio
+			t.references :factura, foreign_key: true
+     			t.string :nombre
+     			t.integer :cantidad
+     			t.integer :precio
 
-     		t.timestamps
+     			t.timestamps
    		end
  	end
 end

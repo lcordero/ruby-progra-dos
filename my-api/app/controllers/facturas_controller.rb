932369af80b15205c6b08adf273f0a5b4273b4d6
isacc46 <isaccmo46@gmail.com>
2019-06-21 01:23:04 +0000

    clases

---
+++ my-api/app/controllers/facturas_controller.rb
@@ -0,0 +1,43 @@
+class FacturasController < ApplicationController
+	  before_action :set_factura, only: [:show, :update, :destroy]
+
+  # GET /facturas
+  def index
+    @facturas = Factura.all
+    json_response(@facturas)
+  end
+
+  # POST /facturas
+  def create
+    @factura = Factura.create!(factura_params)
+    json_response(@factura, :created)
+  end
+
+  # GET /facturas/:id
+  def show
+    json_response(@factura)
+  end
+
+  # PUT /facturas/:id
+  def update
+    @factura.update(factura_params)
+    head :no_content
+  end
+
+  # DELETE /facturas/:id
+  def destroy
+    @factura.destroy
+    head :no_content
+  end
+
+  private
+
+  def factura_params
+    # whitelist params
+    params.permit(:titulo, :creada_por, :fecha, :total)
+  end
+
+  def set_factura
+    @factura = Factura.find(params[:id])
+  end
+end
--- my-api/db/schema.rb
+++ my-api/db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_023717) do
+ActiveRecord::Schema.define(version: 2019_06_20_233905) do
 
   create_table "demos", force: :cascade do |t|
     t.string "title"
     t.string "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "facturas", force: :cascade do |t|
+    t.string "titulo"
+    t.string "creada_por"
+    t.date "fecha"
+    t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -26,6 +35,16 @@ ActiveRecord::Schema.define(version: 2019_06_14_023717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["todo_id"], name: "index_items_on_todo_id"
+  end
+
+  create_table "productos", force: :cascade do |t|
+    t.integer "factura_id"
+    t.string "nombre"
+    t.integer "cantidad"
+    t.integer "precio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["factura_id"], name: "index_productos_on_factura_id"
   end
 
   create_table "tes", force: :cascade do |t|

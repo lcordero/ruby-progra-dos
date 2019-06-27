590da241242c5baac3a981168053a4f478382b72
anthony <thonyram0600@gmail.com>
2019-06-27 17:58:07 -0600

    1er respuesta examen

--- examen-1.rb
+++ examen-1.rb
@@ -1,8 +1,16 @@
 class Animal
     attr_accessor :color,:tamano
-    def Sonido
-        puts "Desconocido"
-    end
+def Sonido
+    puts "Desconocido"
+end
+end
+class Caballo < Animal
+    attr_accessor :Raza
+def Sonido
+    puts "Relincha!"
+end
 end
 a = Animal.new
+c = Caballo.new
 puts a.Sonido
+puts c.Sonido

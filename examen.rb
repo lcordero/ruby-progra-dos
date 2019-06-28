c863bef82d222a63a8eb642233ef8058556ba545
alejandro-z <ik012616@gmail.com>
2019-06-28 01:45:18 +0000

    Examen 06272019 - bsi-515

---
+++ examen.rb
@@ -0,0 +1,20 @@
+
+class Animal
+    def : color,tamaño
+            puts @color
+                 @tamaño
+     metodo:"sonido"
+
+     end
+    puts:"desconocido!"
+end
+
+class Animal<Toro
+    def : raza
+        puts @raza
+
+    metodo:"sonido"
+
+    end
+    puts: "Muu!"
+end
---
+++ modulo.rb
@@ -0,0 +1,16 @@
+Modulo : Notificaciones
+    attr_accessor :nombre, :nombre, :nombre
+        def inicialize(nombre,nombre,nombre)
+            @nombre = "Exito"
+            @nombre = "Alerta"
+            @nombre = "Error"
+
+        end
+
+        funcion : Notificaciones.new("Yeah","Wait","Sorry")
+        puts funcion
+    end
+    cc
+~
+~
+

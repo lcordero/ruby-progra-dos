bf6ee319062df775caeb76319de104fa2f7ece0b
katherinegam <kathi9605@hotmail.com>
2019-07-05 03:04:56 +0000

    Facturas y Productos

--- my-api/app/controllers/productos_controller.rb
+++ my-api/app/controllers/productos_controller.rb
@@ -4,6 +4,8 @@ class ProductosController < ApplicationController
   before_action :set_factura
   before_action :set_factura_producto, only: [:show, :update, :destroy]
 
+  after_action :set_factura_total, only: [:update, :create, :destroy]
+
   # GET /facturas/:factura_id/productos
   def index
     json_response(@factura.productos)
@@ -16,8 +18,19 @@ class ProductosController < ApplicationController
 
   # POST /facturas/:factura_id/productos
   def create
-    @factura.productos.create!(producto_params)
-    json_response(@factura, :created)
+    @producto_temp = @factura.productos.find_by(nombre: producto_params[:nombre])
+   
+
+    if @producto_temp.nil?
+       @factura.productos.create!(producto_params) 
+    else
+       @producto_temp[:cantidad] = @producto_temp[:cantidad] + producto_params[:cantidad].to_i
+       @producto_temp[:precio] = producto_params[:precio]
+       @producto_temp.save
+    end
+
+    # @factura.producto.create!(producto_params)
+    json_response(@factura, :created) 
   end
 
   # PUT /facturas/:factura_id/productos/:id
@@ -44,6 +57,17 @@ class ProductosController < ApplicationController
 
   def set_factura_producto
     @producto = @factura.productos.find_by!(id: params[:id]) if @factura
+  end
+
+  def set_factura_total
+    @factura_sub_total = 0
+
+    @factura.productos.each do |producto|
+      @factura_sub_total = @factura_sub_total + (producto.cantidad * producto.precio)
+    end
+    @factura.total = @factura_sub_total
+
+    @factura.save
   end
 
 end

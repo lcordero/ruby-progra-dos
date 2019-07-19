3e9ca251163f9aa7eb6125ce08b8885c2f4492be
anthony-r <thonyram0600@gmail.com>
2019-07-19 02:02:38 +0000

    proyecto

--- my-api/db/seeds.rb
+++ my-api/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+#
+# es como quemar una factura para no crear de una en una
+  factura1 = Factura.create(titulo: "Test Seed", creada_por: "Team#1", fecha: "28/12/1998", total: "1234")
+  producto1 = Producto.create(factura_id: factura1[:id], nombre: "Prod_Fact1", cantidad: 12, precio: 2000)

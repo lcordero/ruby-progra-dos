129c6bc0d80da4584be9081a04aeb7c32112f987
Chonsos <red_alonso_2001@hotmail.com>
2019-07-19 19:07:39 +0000

    Modificacion de los vi para integrar los specs

--- proyecto/app/models/factura_vacuna.rb
+++ proyecto/app/models/factura_vacuna.rb
@@ -1,3 +1,4 @@
 class FacturaVacuna < ApplicationRecord
 has_many :vacunas, dependent: :destroy
+validates_presence_of :Nombre_del_Cliente :Apellidos_del_Cliente :Cedula :Lugar_de_Residencia :Fecha_de_Nacimiento :Total
 end

82a2c93ce79346c474a8863ee3c1a7ec279c0780
Chonsos <red_alonso_2001@hotmail.com>
2019-07-22 19:36:35 +0000

    xd

--- proyecto/db/migrate/20190719021949_create_vacunas.rb
+++ proyecto/db/migrate/20190719021949_create_vacunas.rb
@@ -1,7 +1,7 @@
 class CreateVacunas < ActiveRecord::Migration[5.2]
   def change
     create_table :vacunas do |t|
-      t.references :factura_vacuna, foreign_key: true
+      t.references :facturavacuna, foreign_key: true
       t.string :Enfermedad
       t.string :Vacuna_Recomendada
       t.integer :Dosis

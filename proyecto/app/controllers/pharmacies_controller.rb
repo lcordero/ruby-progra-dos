ba12058e8cf8ffef65aa2ba38d2e6c6f8f8a3995
alejandro-z <ik012616@gmail.com>
2019-07-22 21:12:35 +0000

    avance final

--- proyecto/app/controllers/pharmacies_controller.rb
+++ proyecto/app/controllers/pharmacies_controller.rb
@@ -3,47 +3,54 @@ class PharmaciesController < ApplicationController
 
 # GET /pharmacies
 	def index
-	@pharmacies = Pharmacy.all
-	json_response(@pharmacys)
-end
+		@pharmacies = Pharmacy.all
+		json_response(@pharmacys)
+        end
 
 # POST /pharmacies
 	def create
-	@pharmacy = Pharmacy.create!(pharmacy_params)
-	json_response(@pharmacy, :created)
-end
+		@pharmacy = Pharmacy.create!(pharmacy_params)
+		json_response(@pharmacy, :created)
+        end
 
 # GET /pharmacies/:id
 	def show
-	json_response(@pharmacy)
-end
+		json_response(@pharmacy)
+	end
 
 # PUT /pharmacies/:id
 	def update
-	@pharmacy.update(pharmacy_params)
-	head :no_content
-end
+		@pharmacy.update(pharmacy_params)
+		head :no_content
+        end
 
 # DELETE /pharmacies/:id
 	def destroy
-	@pharmacy.destroy
-	head :no_content
-end
-	def filter_recipes
-	@filter_recipes = Pharmacy.find(params[:nombre])
-	@temp_recipes = @filter_recipes.recipe
-        	
+		@pharmacy.destroy
+		head :no_content
+        end
+	def filter_recipes     #definir el filtro de recetas
+		@temp_pharmacy =Pharmacy.find(params[:pharmacy_id])	# buscar atravez de farmacia
+		@filter_recipes = params.permit(:nombre)  #aqui lo que se hace es decirle a filter_recipes es 
+		 @result =[]
+		 for recipe in @temp_pharmacy.recipes do 
+			 
+                   @result.push(recipe) if recipe.cliente = params[:nombre]
+		 end
 
-end	
+	         json_response(@result)
+		
+
+	end	
 private
 
-def pharmacy_params
-        # whitelist params
-        params.permit(:nombre)
-end
+	def pharmacy_params
+        	# whitelist params
+        	params.permit(:nombre)
+	end
 
-def set_pharmacy
-        @pharmacy = Pharmacy.find(params[:id])
+	def set_pharmacy
+        	@pharmacy = Pharmacy.find(params[:id])
 	
-end
+	end
 end

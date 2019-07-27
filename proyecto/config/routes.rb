2920db00f99dd33df5df3408996587556512909f
Jairo1123 <jcvv8607@yahoo.es>
2019-07-27 22:23:09 +0000

    error

--- proyecto/config/routes.rb
+++ proyecto/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :inventarios do
-  resources :productos
-  resources :productos
-  resources :inventarios
+   resources :inventarios do
+   resources :productos
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos do
     resources :items

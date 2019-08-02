a6be7f70d9c7f7ec47b211fa69fb8da1c5b44c90
Jairo1123 <jcvv8607@yahoo.es>
2019-08-02 00:30:30 +0000

    inventario

--- proyecto/app/models/inventario.rb
+++ proyecto/app/models/inventario.rb
@@ -1,3 +1,6 @@
 class Inventario < ApplicationRecord
 	has_many :productos, dependent: :destroy
+	validates :sistema, :via, length: { maximum: 15 }
+	validates :sistema, :via, :fecha_ingreso, presence: { message: "Datos necesarios" } 
+	#validates :fecha_ingreso,
 end
--- proyecto/app/models/producto.rb
+++ proyecto/app/models/producto.rb
@@ -1,3 +1,5 @@
 class Producto < ApplicationRecord
     belongs_to :inventario
+    validates :nombre, length: { maximum: 15 }
+    validates :nombre, :cantidad, :fecha_vencimiento, presence: { message: "Datos necesarios" }
 end

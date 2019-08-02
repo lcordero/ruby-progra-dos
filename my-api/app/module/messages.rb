68be895c8e8ec44186f254446e4fafa09bd605b0
Jairo1123 <jcvv8607@yahoo.es>
2019-08-02 01:20:05 +0000

    modulos

---
+++ my-api/app/module/messages.rb
@@ -0,0 +1,18 @@
+module Messages
+
+  def alert(msg='This is an alert')
+      render json: {'msg' : msg}, status: :ok
+  end
+
+  def success(msg='This was success')
+      render json: {'msg' : msg}, status: :ok
+  end
+
+  def error(msg='this is an error')
+      render json: {'msg' : msg}, status: :ok
+  end
+
+  def info(msg='This is just info')
+      render json: {'msg' : msg}, status: :ok
+  end
+end

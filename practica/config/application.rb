d36b5ce16cec46b2edd0455135957bf53e4d52f6
Leandro C <lecona28@gmail.com>
2019-08-08 17:40:48 +0000

    Changes on Application file

--- practica/config/application.rb
+++ practica/config/application.rb
@@ -31,5 +31,6 @@ module Proyecto
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.fallbacks = [I18n.default_locale]
   end
 end

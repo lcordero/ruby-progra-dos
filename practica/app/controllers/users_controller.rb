daaf3eb8011eb54931935692e2c33b8e5f171163
Jairo1123 <jcvv8607@yahoo.es>
2019-08-09 04:28:12 +0000

    practica-exam

---
+++ practica/app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :destroy, :custom]
+
+	# GET /users
+	def index
+	 @users = User.all
+	 json_response(@users)
+	end
+
+	# GET /users/:id
+	def show
+	 json_response(@user)
+	end
+
+	# POST /user
+	def create
+	 @user = User.create!(user_params)
+	 json_response(@user, :created)
+	end
+
+	# DELETE /user
+	def destroy
+	 @user.destroy
+	 head :no_content
+	end
+
+	# GET#custom
+	def custom
+	 alerta = {"Alerta":"Mi usuario: " + @user[:Nombre]}
+	 json_response(alerta)
+	end
+
+	private
+
+	def user_params
+	 params.permit(:Nombre, :Edad,)
+	end
+	
+	def set_user
+		@user = User.find(params[:id])
+	end
+end

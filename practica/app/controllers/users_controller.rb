f5427a87e31ecec4c51eb32b1a08d40235284b62
Davidzb16 <isaaczb12@gmail.com>
2019-08-09 01:43:47 +0000

    Practica

---
+++ practica/app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+before_action :set_user, only: [:show, :update, :destroy, :custom]
+# GET#index/users
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+# GET#show /users/:id
+  def show
+    json_response(@user)
+  end
+# GET#custom/custom/:id
+  def custom
+    alerta = {
+	    "alerta": "Mi usuario:" + @user[:nombre]
+    }
+    json_response(alerta)
+
+  end 
+
+# POST /users
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+# DELETE /users/:id
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+ private
+
+  def user_params
+    # whitelist params
+    params.permit(:nombre, :edad)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
--- practica/config/routes.rb
+++ practica/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos do
     resources :items
+    
   end
+  resources :users
+  get "custom/:id", to: "users#custom"
 end

0ce562d551c09f3d5c71cd5a82a49c350576030a
Jairo1123 <jcvv8607@yahoo.es>
2019-08-11 22:05:10 +0000

    practica

--- practica/config/routes.rb
+++ practica/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos do
     resources :items
   end
+  resources :users
   get "custom/:id", to: "users#custom"
+  get "users/custom/:id", to: "users#custom"
 end

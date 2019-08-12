febc0d890cefc059eb07fbab68fe48ad4315083a
sebas-v <jsvale.guerrero@gmail.com>
2019-08-12 15:43:37 +0000

    Practica

--- practica/db/schema.rb
+++ practica/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_060329) do
+ActiveRecord::Schema.define(version: 2019_08_12_153800) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2019_07_17_060329) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.string "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "nombre"
+    t.string "edad"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

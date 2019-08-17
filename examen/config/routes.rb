7f9b61a015e814da97cfebbe4cfa3cbde4708405
isacc46 <isaccmo46@gmail.com>
2019-08-17 22:18:00 +0000

    guardar

--- examen/config/routes.rb
+++ examen/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :todos do
+   resources :zapatos
+   resources :todos do
+ 
     resources :items
   end
+   
+  get "/zapatito/:id", to: "zapatos#zapatito"
+ 
 end
+
+

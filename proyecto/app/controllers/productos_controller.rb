64213e42824dd7fd2add197dda2fe8e40c50bc06
Jairo1123 <jcvv8607@yahoo.es>
2019-08-19 03:32:01 +0000

    proyecto

--- proyecto/app/controllers/productos_controller.rb
+++ proyecto/app/controllers/productos_controller.rb
@@ -2,17 +2,17 @@ class ProductosController < ApplicationController
   before_action :set_inventario
   before_action :set_inventario_producto, only: [:show, :update, :destroy]
 
+  after_action :set_producto_id, only: [:show, :update, :destroy]
+
   # GET /inventarios/:inventario_id/productos
   def index
     temp_inventario = @inventario.attributes.merge({:productos => @inventario.productos})
-    #.where('created_at BETWEEN 10 AND 11', '13/08/2019' , @selected_date.end_of_day)
-    #json_response(@inventario.productos)
     json_response(temp_inventario)
   end
 
   # GET /inventarios/:inventario_id/productos/:id
   def show
-    json_response(@producto)
+      json_response(@producto)
   end
 
   # POST /inventarios/:inventario_id/productos
@@ -45,5 +45,13 @@ class ProductosController < ApplicationController
 
   def set_inventario_producto
     @producto = @inventario.productos.find_by!(id: params[:id]) if @inventario
+    alerta = ""
+      if @producto.cantidad <= 10
+       alerta = "Este producto esta por acabarse"
+      json_response([alerta] + [@producto])
+      else
+     @producto = @inventario.productos.find_by!(id: params[:id]) if @inventario
+     json_response(@producto)
+      end
   end
 end
--- proyecto/config/routes.rb
+++ proyecto/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
    resources :inventarios do
      resources :productos
+     #get "inventarios/:id/productos/:id", controller: "productos", action: :alerta_productos 
    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos do

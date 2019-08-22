cc4b48c9116e8c943a5b38b27fb79ef0395cf96b
Jairo1123 <jcvv8607@yahoo.es>
2019-08-22 23:18:34 +0000

    proyecto

--- proyecto/app/models/inventario.rb
+++ proyecto/app/models/inventario.rb
@@ -2,5 +2,11 @@ class Inventario < ApplicationRecord
 	has_many :productos, dependent: :destroy
 	validates :sistema, :via, length: { maximum: 15 }
 	validates :sistema, :via, :fecha_ingreso, presence: { message: "Datos necesarios" } 
-	#validates :fecha_ingreso,
 end
+	#validates :fecha_ingreso > Date.today {message: "la fecha no puede ser en el pasdo"}
+	#def fecha_ingreso
+    #if fecha_ingreso.present? && fecha_ingreso < Date.today
+     # errors.add(:fecha_ingreso, "No puede ser en el pasado")
+    #end
+  #end
+  #end

8de64807433dcac2e8e717a60a2d44927e96c601
Jairo1123 <jcvv8607@yahoo.es>
2019-08-22 21:16:44 +0000

    proyecto

--- proyecto/db/migrate/20190722220455_create_inventarios.rb
+++ proyecto/db/migrate/20190722220455_create_inventarios.rb
@@ -4,6 +4,7 @@ class CreateInventarios < ActiveRecord::Migration[5.2]
       t.string :sistema
       t.string :via
       t.date :fecha_ingreso
+      t.integer :total_productos
 
       t.timestamps
     end

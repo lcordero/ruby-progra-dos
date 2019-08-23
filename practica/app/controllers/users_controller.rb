ecc8f188fc7627a102667221451e4794c77cc92b
katherinegam <kathi9605@hotmail.com>
2019-08-23 12:53:47 +0000

    práctica subida

--- practica/app/controllers/users_controller.rb
+++ practica/app/controllers/users_controller.rb
@@ -1,52 +1,45 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :update, :destroy]
-	
-# GET #index /User
-def index 
-  @user = User.all
-  json_response(@user)
+  before_action :set_user, only: [:show, :update, :destroy]
 
-end
+  # GET index/users
+  def index
+    @users = User.all
+    json_response(@users)
+  end
 
-# GET #SHOW /User
-def show
-   @user = User.find(params[:id])
-   json_response(@user)
+  # POST /users
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
 
-end
+  # GET /users/:id
+  def show
+    json_response(@user)
+  end
 
+  # PUT /users/:id
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
 
-# POST /USERS
-def create 
-   @user = User.create!(user_params)
-   json_response(@user, :created)
-
-end
-
-
-# Delete /User
-def destroy 
+  # DELETE /users/:id
+  def destroy
     @user.destroy
     head :no_content
-end
-
-# GET #CUSTOM /User
-def custom
- my_variable = {
-	 "alerta": "mi user: "+@user[:Nombre]
- }
- json_response(my_variable)
-
-end
+  end
 
   private
 
-def user_params
- params.permit(:Nombre, :edad)
-end
-def set_user
- @user = User.find(params[:id])
+  def user_params
+    # whitelist params
+    params.permit(:nombre, :edad)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
-   
-end
+
